8c43d95d525310e85737d0cd3ebc5cc84004437a
MYasin99 <Mahmood.Yasin@edu.sait.ca>
2023-09-15 14:18:14 -0600

    week 2 assignment

--- app/page.js
+++ app/page.js
@@ -1,8 +1,14 @@
+import Link from 'next/link';
+import StudentInfo from './studentinfo.jsx';
+
 export default function Page() {
   return (
     <main>
-      <h1>Hello World!</h1>
-      <p>This is a paragraph.</p>
+      <h1>CPRG 306: Web Development 2 - Assignments</h1>
+      <StudentInfo/>
+      <p>
+      <Link href="/week2"> Week 2 </Link>
+      </p>
     </main>
   )
 }
