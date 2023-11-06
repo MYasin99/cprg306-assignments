b8ca8b486ede506284de8b1162062caae23e164d
MYasin99 <Mahmood.Yasin@edu.sait.ca>
2023-11-06 02:55:18 -0700

    Update page.js

--- app/week8/page.js
+++ app/week8/page.js
@@ -1,11 +1,31 @@
+"use client";
+
+import React from 'react';
 import { useUserAuth } from "./_utils/auth-context";
- 
-const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
- 
-await gitHubSignIn();
- 
-await firebaseSignOut();
- 
-<p>
-  Welcome, {user.displayName} ({user.email})
-</p>;
+
+const Page = () => {
+  const { user, gitHubSignIn } = useUserAuth();
+
+  const handleSignIn = async () => {
+    try {
+      await gitHubSignIn();
+    } catch (error) {
+      console.error('Error signing in with GitHub:', error);
+    }
+  };
+
+  if (user) {
+    // User is signed in, display welcome message
+    return <p>Welcome, {user.displayName} ({user.email})</p>;
+  } else {
+    // User is not signed in, display sign in button
+    return (
+      <div>
+        <p>Please sign in to access your shopping list:</p>
+        <button onClick={handleSignIn}>Sign in with GitHub</button>
+      </div>
+    );
+  }
+};
+
+export default Page;
