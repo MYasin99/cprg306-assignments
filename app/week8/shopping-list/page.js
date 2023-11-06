d6cca2247766499b7ff1ee9ee355a90d4e6a333e
MYasin99 <Mahmood.Yasin@edu.sait.ca>
2023-11-06 02:41:34 -0700

    week8 - almost done

--- app/week8/shopping-list/page.js
+++ app/week8/shopping-list/page.js
@@ -1,14 +1,25 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import NewItem from './new-item';
 import ItemList from './item-list';
 import MealIdeas from './meal-ideas';
 import itemsData from './items.json';
+import { useUserAuth } from './_utils/auth-context'; // Update this path as necessary
 
 const Page = () => {
   const [items, setItems] = useState(itemsData);
   const [selectedItemName, setSelectedItemName] = useState('');
+  const { user } = useUserAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    // If the user is not logged in, redirect to the landing page
+    if (!user) {
+      router.push('/landing'); // Replace '/landing' with the path to your landing page
+    }
+  }, [user, router]);
 
   const handleAddItem = (item) => {
     setItems(prevItems => [...prevItems, item]);
@@ -18,6 +29,11 @@ const Page = () => {
     const cleanedItemName = itemName.split(',')[0].trim().replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|�[�-�]|�[�-�]|[\u2011-\u26FF]|�[�-�])/g, '');
     setSelectedItemName(cleanedItemName);
   };
+
+  // Only render the page content if the user is logged in
+  if (!user) {
+    return null; // Or a <Loading/> component or any other placeholder
+  }
 
   return (
     <main className="max-w-3xl mx-auto p-4">
